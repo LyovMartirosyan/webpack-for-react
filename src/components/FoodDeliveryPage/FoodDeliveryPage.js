64f3aad64c781b17068a9a0c8c7c63f2c01d906f
Hakob_Tagayan <44642041+Tagayan061994@users.noreply.github.com>
2022-08-08 00:24:10 +0400

    datan tara Restaurants i mej
    datan tara Restaurants i mej

--- src/components/FoodDeliveryPage/FoodDeliveryPage.js
+++ src/components/FoodDeliveryPage/FoodDeliveryPage.js
@@ -8,60 +8,9 @@ import img7 from "../images/meat.png";
 import img8 from "../images/sushi.png";
 import img9 from "../images/broccoli.png";
 import img10 from "../images/cake.png";
-import img11 from "../images/restaurant1.png";
-import img12 from "../images/restaurant2.png";
-import img13 from "../images/restaurant3.png";
-import img14 from "../images/restaurant4.png";
-import img15 from "../images/restaurant5.png";
-import img16 from "../images/restaurant6.png";
-import img17 from "../images/pizza_mini.png";
-import img18 from "../images/sushi_mini.png";
+
 import Restaurants from "../Restaurants";
 
-const data = [
-  {
-    url1: img11,
-    url2: img17,
-    title: "Royal Sushi House",
-    infoText: "0-40 min ○ $32 min sum",
-    labelText: "sushi",
-  },
-  {
-    url1: img12,
-    url2: img18,
-    title: "Burgers & Pizza",
-    infoText: "0-40 min ○ $32 min sum",
-    labelText: "Pizza & burger",
-  },
-  {
-    url1: img13,
-    url2: img17,
-    title: "Ninja sushi",
-    infoText: "0-40 min ○ $32 min sum",
-    labelText: "sushi",
-  },
-  {
-    url1: img12,
-    url2: img17,
-    title: "Pizzeria",
-    infoText: "0-40 min ○ $32 min sum",
-    labelText: "pizza",
-  },
-  {
-    url1: img11,
-    url2: img17,
-    title: "Royal Sushi House",
-    infoText: "0-40 min ○ $32 min sum",
-    labelText: "sushi",
-  },
-  {
-    url1: img11,
-    url2: img17,
-    title: "Royal Sushi House",
-    infoText: "0-40 min ○ $32 min sum",
-    labelText: "sushi",
-  },
-];
 
 const FoodDeliveryPage = () => {
   return (
@@ -116,7 +65,7 @@ const FoodDeliveryPage = () => {
           {/* move this part to another component */}
         </div>
         <div>
-          <Restaurants data={data} />
+          <Restaurants />
         </div>
       </main>
     </>
--- src/components/Restaurants/Restaurants.js
+++ src/components/Restaurants/Restaurants.js
@@ -1,9 +1,61 @@
 import React from "react";
 import styles from "./styles.css";
+import img11 from "../images/restaurant1.png";
+import img12 from "../images/restaurant2.png";
+import img13 from "../images/restaurant3.png";
+import img14 from "../images/restaurant4.png";
+import img15 from "../images/restaurant5.png";
+import img16 from "../images/restaurant6.png";
+import img17 from "../images/pizza_mini.png";
+import img18 from "../images/sushi_mini.png";
 
-const Restaurants = (props) => {
-  const { data } = props;
+const data = [
+  {
+    url1: img11,
+    url2: img17,
+    title: "Royal Sushi House",
+    infoText: "0-40 min ○ $32 min sum",
+    labelText: "sushi",
+  },
+  {
+    url1: img12,
+    url2: img18,
+    title: "Burgers & Pizza",
+    infoText: "0-40 min ○ $32 min sum",
+    labelText: "Pizza & burger",
+  },
+  {
+    url1: img13,
+    url2: img17,
+    title: "Ninja sushi",
+    infoText: "0-40 min ○ $32 min sum",
+    labelText: "sushi",
+  },
+  {
+    url1: img12,
+    url2: img17,
+    title: "Pizzeria",
+    infoText: "0-40 min ○ $32 min sum",
+    labelText: "pizza",
+  },
+  {
+    url1: img11,
+    url2: img17,
+    title: "Royal Sushi House",
+    infoText: "0-40 min ○ $32 min sum",
+    labelText: "sushi",
+  },
+  {
+    url1: img11,
+    url2: img17,
+    title: "Royal Sushi House",
+    infoText: "0-40 min ○ $32 min sum",
+    labelText: "sushi",
+  },
+];
 
+
+const Restaurants = () => {
   return (
     <div className={styles.restaurants}>
       {data.map((item, index) => {
